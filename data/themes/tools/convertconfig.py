1056558bb6f8ae1b9b5d89c9f8b3bec76bf82236
Nick Espig <nickespig@gmail.com>
2017-06-09 15:48:30 +0200

    add brackets to print to make it python3 compatilbe

--- data/themes/tools/convertconfig.py
+++ data/themes/tools/convertconfig.py
@@ -6,18 +6,19 @@ import sys
 try:
     path = sys.argv[1]
 except:
-    print "usage: convertconfig.py config.txt"
-    print ""
-    print "Converts the old PGU theme config.txt format into the style.ini format"
-    print ""
+    print("usage: convertconfig.py config.txt")
+    print("")
+    print("Converts the old PGU theme config.txt format into the style.ini format")
+    print("")
     sys.exit(1)
 
 styleByClass = collections.defaultdict(dict)
 
-fd = file(path, "r")
+fd = open(path, "r")
 for line in fd.readlines():
     line = line.strip()
-    if (not line): continue
+    if not line:
+        continue
 
     args = line.split()
     try:
@@ -25,14 +26,13 @@ for line in fd.readlines():
         attr = args[1]
         values = " ".join(args[2:])
     except:
-        print "Invalid line: %s" % line
+        print("Invalid line: %s" % line)
         sys.exit()
     styleByClass[name][attr] = values
 fd.close()
 
 for klass in sorted(styleByClass.keys()):
-    print "[%s]" % klass
+    print("[%s]" % klass)
     for attr in sorted(styleByClass[klass].keys()):
-        print "%s = %s" % (attr, styleByClass[klass][attr])
-    print ""
-
+        print("%s = %s" % (attr, styleByClass[klass][attr]))
+    print("")
